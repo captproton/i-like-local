d39808453652476ac16327f2aefbea9ac7218ff5
Carl Tanner <captproton@betterbilling.net>
2008-02-02 07:37:07 -0800

    Deleted Tables no longer support models or controllers

--- db/migrate/027_add_habitat_id.rb
+++
@@ -1,9 +0,0 @@
-class AddHabitatId < ActiveRecord::Migration
-  def self.up
-        add_column :foreclosures, :habitat_id, :integer
-  end
-
-  def self.down
-        remove_column :foreclosures, :habitat_id
-  end
-end
--- db/migrate/028_add_user_id.rb
+++
@@ -1,9 +0,0 @@
-class AddUserId < ActiveRecord::Migration
-  def self.up
-        add_column :user_profiles, :user_id, :integer
-  end
-
-  def self.down
-        remove_column :user_profiles, :user_id
-  end
-end
